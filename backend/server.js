6bc226214b8dc36c0e17309e7753bc781633668c
Mahmoud Ahmed Metwally <113688967+MahmoudMetwally2699@users.noreply.github.com>
2025-07-07 12:04:54 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const allowedOrigins = [
   'https://gaith-tours-backend.vercel.app',
   'https://gaithtours.com',
   'https://api.gaithtours.com',
+  'https://www.gaithtours.com'
   process.env.FRONTEND_URL,
   process.env.BACKEND_URL
 ].filter(Boolean);
